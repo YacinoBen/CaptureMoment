bd03c125f275bdc8d3aebec6184a69cd44065357
Yacine Benaffane <yacine.benaffane@gmail.com>
2025-12-06 16:05:14 +0100

    add core/type.h a Entry point for all core type definitions

---
+++ core/include/types.h
@@ -0,0 +1,29 @@
+/**
+ * @file types.h
+ * @brief Entry point for all core type definitions
+ * @author CaptureMoment Team
+ * @date 2025
+ * 
+ * This file aggregates all the basic types of the project.
+ * Include this file instead of including each header individually.
+ * 
+ * @code
+ * #include "types.h"
+ * @endcode
+ */
+
+#pragma once
+
+#include "pixel_format.h"
+#include "image_region.h"
+
+/**
+ * @namespace CaptureMoment
+ * @brief Main namespace for the CaptureMoment project
+ * 
+ * All classes, structures, and functions of the project are within
+ * this namespace to avoid name conflicts.
+ */
+namespace CaptureMoment {
+    // Types are defined in individual headers (see above)
+}
