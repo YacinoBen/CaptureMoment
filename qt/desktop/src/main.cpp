7d2ebeae60d4efffd451966aee3d4c1946853f3f
Yacine Benaffane <yacine.benaffane@gmail.com>
2025-12-15 21:49:35 +0100

    optimize main.cpp in qt/desktop

--- qt/desktop/src/main.cpp
+++ qt/desktop/src/main.cpp
@@ -1,6 +1,8 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include "utils/qml_context_setup.h"
+#include "rendering/rhi_image_item.h"
+#include <spdlog/spdlog.h>
 
 int main(int argc, char *argv[])
 {
@@ -8,6 +10,20 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
 
     CaptureMoment::UI::QmlContextSetup::setupContext(engine.rootContext());
+    qmlRegisterType<CaptureMoment::UI::Rendering::RHIImageItem>("CaptureMoment.Qt.Rendering", 1, 0, "RHIImageItem");
+
+    auto context = engine.rootContext();
+    if (!CaptureMoment::UI::QmlContextSetup::setupContext(context)) {
+        spdlog::error("Failed to setup QML context");
+        return -1;
+    }
+
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreationFailed,
+        &app,
+        []() { QCoreApplication::exit(-1); },
+        Qt::QueuedConnection);
 
     engine.loadFromModule("CaptureMoment.desktop", "DesktopMain");
 
