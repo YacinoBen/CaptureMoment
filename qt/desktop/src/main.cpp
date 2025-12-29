d876a250ee248871cd18c5a1758ff39a0f81abd4
Yacine Benaffane <yacine.benaffane@gmail.com>
2025-12-30 00:58:06 +0100

    add rhi in main.cpp

--- qt/desktop/src/main.cpp
+++ qt/desktop/src/main.cpp
@@ -2,9 +2,10 @@
 #include <QQmlApplicationEngine>
 
 #include "utils/qml_context_setup.h"
-//#include "rendering/qml_rhi_image_item.h"
-#include "rendering/qml_sgs_image_item.h"
+
 #include "rendering/qml_painted_image_item.h"
+#include "rendering/qml_sgs_image_item.h"
+#include "rendering/qml_rhi_image_item.h"
 
 #include <spdlog/spdlog.h>
 
@@ -13,12 +14,15 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
-    // Register QML types
+    // Register QML types Rendering
     qmlRegisterType<CaptureMoment::UI::QMLPaintedImageItem>(
         "CaptureMoment.UI.Rendering.Painted", 1, 0, "QMLPaintedImageItem"
     );
     qmlRegisterType<CaptureMoment::UI::QMLSGSImageItem>(
         "CaptureMoment.UI.Rendering.SGS", 1, 0, "QMLSGSImageItem"
+        );
+    qmlRegisterType<CaptureMoment::UI::QMLRHIImageItem>(
+        "CaptureMoment.UI.Rendering.RHI", 1, 0, "QMLRHIImageItem"
         );
 
     // Setup QML context once
