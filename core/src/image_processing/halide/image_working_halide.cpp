3d9f4c0667a51b3304918aa6e99c6809b6fc3706
Yacine Benaffane <yacine.benaffane@gmail.com>
2026-01-22 23:53:54 +0100

    Core::ImagePRocessing add ImageWorkingHalide. Base class for Halide-based working image implementations.

---
+++ core/src/image_processing/halide/image_working_halide.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file working_image_halide.cpp
+ * @brief Implementation of WorkingImageHalide
+ * @author CaptureMoment Team
+ * @date 2026
+ */
+
+#include "image_processing/working_image_halide.h"
+#include <spdlog/spdlog.h>
+#include <memory>
+#include <cstring>
+
+namespace CaptureMoment::Core::ImageProcessing {
+
+void WorkingImageHalide::initializeHalide(const Common::ImageRegion& working_image)
+{
+    if (m_data.empty()) {
+        spdlog::error("WorkingImageHalide::initializeHalide: Internal data vector is empty, cannot initialize buffer");
+        return;
+    }
+
+    m_halide_buffer = Halide::Buffer<float>(
+        m_data.data(),
+        working_image.m_width,
+        working_image.m_height,
+        working_image.m_channels
+        );
+}
+
+} // namespace CaptureMoment::Core::ImageProcessing
