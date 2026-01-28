7412d2c3e16bb0115433419e6a02ef55b2984e7e
Yacine Benaffane <yacine.benaffane@gmail.com>
2026-01-28 16:41:23 +0100

    Core::Operations::OperationPipeline: operation_pipeline.h use  return std::expected<void, ErrorHandling::CoreError> applyOperations

--- core/include/operations/operation_pipeline.h
+++ core/include/operations/operation_pipeline.h
@@ -9,6 +9,7 @@
 
 #include <vector>
 #include "image_processing/interfaces/i_working_image_hardware.h"
+#include "common/error_handling/core_error.h"
 
 namespace CaptureMoment::Core {
 
@@ -33,20 +34,12 @@ public:
     /**
      * @brief Applies a sequence of operations to a working image.
      *
-     * Iterates through the provided list of OperationDescriptors, uses the
-     * given OperationFactory to create the corresponding operation instances,
-     * and executes them sequentially on the input/output working image.
-     * The working image is modified in-place.
-     *
-     * @param[in,out] working_image The working image to process. This object is modified
-     *                              by the operations.
-     * @param[in] operations A vector of OperationDescriptor objects defining
-     *                       the sequence of operations to apply.
-     * @param[in] factory The OperationFactory instance used to create the
-     *                    concrete operation objects.
-     * @return true if all operations were applied successfully, false otherwise.
+     * @param[in,out] working_image The working image to process.
+     * @param[in] operations A vector of OperationDescriptor objects.
+     * @param[in] factory The OperationFactory instance.
+     * @return std::expected<void, CoreError> Success or the error code if an operation failed.
      */
-    [[nodiscard]] static bool applyOperations(
+    [[nodiscard]] static std::expected<void, ErrorHandling::CoreError> applyOperations(
         ImageProcessing::IWorkingImageHardware& working_image,
         const std::vector<OperationDescriptor>& operations,
         const OperationFactory& factory
--- core/src/operations/operation_pipeline.cpp
+++ core/src/operations/operation_pipeline.cpp
@@ -1,10 +1,3 @@
-/**
- * @file operation_pipeline.cpp
- * @brief Implementation of OperationPipeline
- * @author CaptureMoment Team
- * @date 2026
- */
-
 #include "operations/operation_pipeline.h"
 #include "operations/operation_factory.h"
 #include "operations/operation_descriptor.h"
@@ -13,37 +6,43 @@
 
 namespace CaptureMoment::Core::Operations {
 
-bool OperationPipeline::applyOperations(
+std::expected<void, ErrorHandling::CoreError> OperationPipeline::applyOperations(
     ImageProcessing::IWorkingImageHardware& working_image,
     const std::vector<OperationDescriptor>& operations,
     const OperationFactory& factory
     )
 {
     spdlog::info("OperationPipeline::applyOperations: Starting with {} operations", operations.size());
-    for (const auto& descriptor : operations) {
+
+    for (const auto& descriptor : operations)
+    {
         if (!descriptor.enabled) {
             spdlog::trace("OperationPipeline::applyOperations: Skipping disabled operation '{}'", descriptor.name);
             continue;
         }
 
         spdlog::info("OperationPipeline::applyOperations: Creating operation '{}'", descriptor.name);
-        auto operation = factory.create(descriptor);
-        if (!operation) {
+
+        auto result_op = factory.create(descriptor);
+        if (!result_op) {
             spdlog::error("OperationPipeline::applyOperations: Failed to create operation '{}'", descriptor.name);
-            return false;
+            return std::unexpected(result_op.error());
         }
+
+        auto& operation = result_op.value();
 
         spdlog::debug("OperationPipeline::applyOperations: Executing operation '{}'", descriptor.name);
 
         if (!operation->execute(working_image, descriptor)) {
             spdlog::error("OperationPipeline::applyOperations: Operation '{}' failed", descriptor.name);
-            return false;
+            return std::unexpected(ErrorHandling::CoreError::InvalidWorkingImage);
         }
+
         spdlog::info("OperationPipeline::applyOperations: Operation '{}' completed", descriptor.name);
     }
 
     spdlog::info("OperationPipeline::applyOperations: All operations completed successfully");
-    return true;
+    return {};
 }
 
 } // namespace CaptureMoment::Core::Operations
