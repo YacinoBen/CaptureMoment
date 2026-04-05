dcc0c6a743c6e6272b0ae42b30fce1554af6b9e5
Yacine Benaffane <yacine.benaffane@gmail.com>
2026-04-05 11:52:49 +0200

    Core::Utils: add umbrella header utils.h

---
+++ core/include/utils/utils.h
@@ -0,0 +1,31 @@
+/**
+ * @file utils.h
+ * @brief Umbrella header for the Utils module of the CaptureMoment Core Library.
+ *
+ * This header provides a single entry point for the `CaptureMoment::Core::Utils` namespace.
+ * Including this file grants access to all fundamental data structures, types,
+ * and utilities defined within the Utils module.
+ *
+ * @author CaptureMoment Team
+ * @date 2026
+ */
+
+#pragma once
+
+/** 
+ * @brief Color Space Utilities
+ */
+
+#include "utils/color_space_utils.h"
+
+/**
+ * @brief Image Conversion Utilities
+ */
+#include "utils/image_conversion.h"
+
+/**
+ * @brief Definition of supported pixel formats (e.g., RGBA_U8, RGBA_F32).
+ */
+#include "utils/to_string_utils.h"
+
+// Add other common headers as needed
