0cd86fd7888cd29b18320409f77e2c7827f1cd7f
Yacine Benaffane <yacine.benaffane@gmail.com>
2026-05-15 20:04:17 +0200

    Core::Common::image_region.h: add new constructor ImageRegion(std::span<const float> data_span, ImageDim w, ImageDim h, ImageChan ch)

--- core/include/common/image_region.h
+++ core/include/common/image_region.h
@@ -127,6 +127,31 @@ struct ImageRegion {
     {}
 
     /**
+     * @brief Constructs an ImageRegion by copying data from a span (Deep Copy).
+     *
+     * @details
+     * This constructor is essential when working with non-owning views (std::span).
+     * It allocates a new internal vector and copies the data from the span.
+     * This is the standard way to export data from a WorkingImage (which works on spans)
+     * back to a standalone ImageRegion.
+     *
+     * @param data_span Non-owning view over the pixel data to copy.
+     * @param w Width in pixels.
+     * @param h Height in pixels.
+     * @param ch Number of color channels per pixel.
+     *
+     * @note The x and y coordinates default to (0, 0).
+     * @note Format defaults to PixelFormat::RGBA_F32.
+     */
+    ImageRegion(std::span<const float> data_span, ImageDim w, ImageDim h, ImageChan ch)
+        : m_data(data_span.begin(), data_span.end())
+        , m_width(w)
+        , m_height(h)
+        , m_channels(ch)
+        , m_format(PixelFormat::RGBA_F32)
+    {}
+
+    /**
      * @brief Constructs an ImageRegion with position and moved pixel data.
      *
      * @param x X-coordinate offset in the source image.
