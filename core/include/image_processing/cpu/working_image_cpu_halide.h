3de3f07dd97eab0bf2285dac11ac9dd2bf623456
Yacine Benaffane <yacine.benaffane@gmail.com>
2026-06-13 13:55:35 +0200

    Core::ImageProcessing::working_image_cpu_halide.h: Remove updateFromCPU(), and improve comments

--- core/include/image_processing/cpu/working_image_cpu_halide.h
+++ core/include/image_processing/cpu/working_image_cpu_halide.h
@@ -33,23 +33,22 @@ public:
 
     /**
      * @brief Binds the view and initializes the zero-copy Halide buffer.
+     * @return true if the view is valid and Halide buffer initialized successfully.
      */
     [[nodiscard]] bool bindView(const ImageView& view) override;
 
-    /**
-     * @brief No-op for CPU Halide.
-     * @details Since the Halide buffer is bound directly to the Context's RAM via 
-     * the span in bindView(), any changes to the CPU data are automatically reflected. 
-     * No re-initialization is required.
-     */
-    [[nodiscard]] std::expected<void, ErrorHandling::CoreError>
-    updateFromCPU() override;
 
+    /**
+     * @brief Exports current internal image data to a new CPU-based ImageRegion..
+     *
+     * @return std::expected<std::unique_ptr<Common::ImageRegion>,  ErrorHandling::CoreError>
+     */
     [[nodiscard]] std::expected<std::unique_ptr<Common::ImageRegion>,  ErrorHandling::CoreError>
     exportToCPUCopy() override;
 
     /**
      * @brief Checks if the CPU view AND the Halide buffer are valid.
+     * @return true if both the CPU view and Halide buffer are valid.
      */
     [[nodiscard]] bool isValid() const override { return  WorkingImageCPU::isValid() && isHalideBufferValid(); };
 private:
