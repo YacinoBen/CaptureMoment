c9d9a73d2d55a610e4d9eed54074baf4e1efc866
Yacine Benaffane <yacine.benaffane@gmail.com>
2026-06-16 16:22:13 +0200

    Core::ImageProcessing::WorkingImageGPU: working_image_gpu.h: clean include, add exportToCPUCopy(), add virtual transferToVRAM(). add virtual downloadDeviceToHost(), improve Comment. working_image_gpu.cpp: implement exportToCPUCopy()

--- core/src/image_processing/gpu/working_image_gpu.cpp
+++ core/src/image_processing/gpu/working_image_gpu.cpp
@@ -16,4 +16,38 @@ bool WorkingImageGPU::isValid() const
     return !m_view_data_image.working_data.empty() && m_view_data_image.width > 0;
 }
 
+std::expected<std::unique_ptr<Common::ImageRegion>, ErrorHandling::CoreError>
+WorkingImageGPU::exportToCPUCopy()
+{
+    if (!isValid()) {
+        return std::unexpected(ErrorHandling::CoreError::InvalidWorkingImage);
+    }
+
+    // Download the GPU buffer to Host RAM (m_view_data_image.working_data)
+    if (!downloadDeviceToHost()) {
+        return std::unexpected(ErrorHandling::CoreError::InvalidWorkingImage);
+    }
+
+    try {
+        // Create a deep copy of the working data into a new ImageRegion
+        auto cpu_image_copy{std::make_unique<Common::ImageRegion>()};
+        cpu_image_copy->m_data.assign(m_view_data_image.working_data.begin(), m_view_data_image.working_data.end());
+
+        cpu_image_copy->m_width = static_cast<int>(m_view_data_image.width);
+        cpu_image_copy->m_height = static_cast<int>(m_view_data_image.height);
+        cpu_image_copy->m_channels = static_cast<int>(m_view_data_image.channels);
+        cpu_image_copy->m_format = Common::PixelFormat::RGBA_F32;
+
+        if (!cpu_image_copy->isValid()) {
+            return std::unexpected(ErrorHandling::CoreError::InvalidImageRegion);
+        }
+
+        return cpu_image_copy;
+    } catch (const std::bad_alloc&) {
+        return std::unexpected(ErrorHandling::CoreError::AllocationFailed);
+    } catch (const std::exception& e) {
+        return std::unexpected(ErrorHandling::CoreError::Unexpected);
+    }
+}
+
 } // namespace CaptureMoment::Core::ImageProcessing
