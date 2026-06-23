81ad8b4458165dd7980d51c41717820e4b1cb91f
Yacine Benaffane <yacine.benaffane@gmail.com>
2026-06-23 13:28:01 +0200

    Core::Pipeline::OperationPipelineExecutorCPU: add OperationPipelineExecutorCPU inherit from OperationPipelineExecutor

---
+++ core/include/pipeline/operations/operation_pipeline_executor_cpu.h
@@ -0,0 +1,41 @@
+/**
+ * @file operation_pipeline_executor_cpu.h
+ * @brief Declaration of OperationPipelineExecutorCPU (Fused Adjustment Pipeline).
+ *
+ * @details
+ * This class is a concrete implementation of `OperationPipelineExecutor` optimized for CPU execution.
+ *
+ * @author CaptureMoment Team
+ * @date 2026
+ */
+
+#pragma once
+
+#include "pipeline/operations/operation_pipeline_executor.h"
+
+namespace CaptureMoment::Core {
+
+namespace Pipeline {
+
+/**
+ * @class OperationPipelineExecutorCPU
+ * @brief Concrete executor for fused Halide adjustment pipelines. CPU
+ *
+ * @details
+ * This executor chains multiple image adjustment operations (Brightness, Contrast, etc.)
+    * into a single Halide graph. It relies on the `IHalidePipelineExecutor` base class
+ * to provide the standardized 4-channel input parameter (`m_input`).
+ */
+class OperationPipelineExecutorCPU final : public OperationPipelineExecutor {
+public:
+    /**
+     * @brief Destructor.
+     */
+    virtual ~OperationPipelineExecutorCPU() = default;
+
+    [[nodiscard]] bool execute(ImageProcessing::IWorkingImageHardware& working_image) override;
+protected:
+    void applyScheduling(Halide::Func& pipeline, Halide::Var& x, Halide::Var& y) const override;
+};
+} // namespace Pipeline
+} // namespace CaptureMoment::Core
