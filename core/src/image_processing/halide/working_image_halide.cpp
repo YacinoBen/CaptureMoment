f06b73b8981b430c46c4d5459acf5a358e519e11
Yacine Benaffane <yacine.benaffane@gmail.com>
2026-07-07 16:25:04 +0200

    Core::ImageProcessing::WorkingImageHalide and WorkingImageCPU_Halide and WorkingImageGPU_Halide: Change representation image from planar by default to  chunky with make_interleaved. Change stride in downsample gpu

--- core/src/image_processing/halide/working_image_halide.cpp
+++ core/src/image_processing/halide/working_image_halide.cpp
@@ -19,7 +19,8 @@ void WorkingImageHalide::initializeHalide(std::span<float> data, Common::ImageDi
 
     // Create Halide Buffer View (Zero-Copy)
     // std::span::data() returns the underlying pointer safely.
-    m_halide_buffer = Halide::Buffer<float>(data.data(), width, height, channels);
+    // Transform from Chunky OIIO to Plannar Halide layout is handled by Halide::Buffer<float>::make_interleaved.
+    m_halide_buffer = Halide::Buffer<float>::make_interleaved(data.data(), width, height, channels);
 
     if (!m_halide_buffer.defined()) {
         spdlog::error("[WorkingImageHalide::initializeHalide]: Failed to define Halide::Buffer.");
